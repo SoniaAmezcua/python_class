92d117cafa778089fd46900aed70ac7969728ad1
Sonia <edithnash20@gmail.com>
2021-02-27 13:00:02 -0600

    Clase 03

---
+++ hola_qa_minds.py
@@ -0,0 +1,120 @@
+"""import this
+
+print('Hola QA Minds')
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+print(c + (a + b) + d)
+print(a + b * (c + d))
+print(a - b * c + d)
+print(((a + b) ** c) -d) """
+
+"""
+print("Bienvenidos \"QA\" Minds")
+print("\"Minds\"!!!")
+print("\"I Love \n Python \n '<3' \"")
+
+print(type(bool(12.4232)))
+print(bool(12.4232))
+
+print(type(bool(123021)))
+print(bool(123021))
+
+a = 12.5
+b = 34
+print (a + b)
+print (a - b)
+print (a * b)
+print (b / a)
+print (int(b % a))
+
+text = '34242'
+print(int(text))
+print(float(text))
+
+
+print(3/False)"""
+
+var_int = 2
+var_float = 2.0
+var_str = "Hola"
+var_bool = True
+var_none = None #=null espacio en memoria que no es nada, no es nada
+
+#print(var_int == var_float) #True
+#print(var_int == var_bool) #True
+#print(0 == False) #True
+#print(0 == var_str) 
+#print(var_str > var_none) 
+#print(0 == var_none) #False
+#print(1 == var_none) #False 
+#print(isinstance(var_int, int)) #False 
+"""
+billetera = 200
+precio_chocolate = 150
+precio_agua = 100
+
+costo_total = precio_chocolate + precio_agua
+if billetera <= costo_total:  
+    print("Monto disponible: {} Costo Total es : {}, \
+        No puedes adquir tu producto".format(billetera, costo_total))
+else:
+    print("Monto disponible: {} Costo Total es : {}, \
+        Puedes adquir tu producto".format(billetera, costo_total))
+
+if billetera <= precio_chocolate:  
+    print("Monto disponible: {} Costo Total es : {}, \
+        No puedes adquir tu producto".format(billetera, precio_chocolate))
+else:
+    print("Monto disponible: {} Costo Total es : {}, \
+        Puedes adquir tu producto".format(billetera, precio_chocolate))
+
+"""
+
+"""
+numero_mes = 0
+
+if numero_mes in (3, 4, 5):
+    print("Primavera")
+elif numero_mes in (6, 7, 8):
+    print("Verano")
+elif numero_mes in (9, 10, 11):
+    print("Otoño")
+elif numero_mes in (12, 1, 2):
+    print("Invierno")  
+else:
+    print("El mes ingresado no existe.\nPor favor verifique e intente de nuevo.\n'La Gerencia'")
+"""
+"""
+edad_permitida = 14
+altura_permitida = 1.62
+
+edad_persona = 12
+altura_persona =1.65
+
+if edad_persona >= edad_permitida and  altura_persona >= altura_permitida:
+    resultado = "Puedes subir a la montaña rusa"
+elif edad_persona < edad_permitida and  altura_persona < altura_permitida:
+    resultado = "No puedes subir por no tener la edad y altura permitida"
+elif edad_persona < edad_permitida:
+    resultado = "No puedes subir por no tener la edad permitida"
+else:
+    resultado = "No puedes subir por no tener la edad y altura permitida"
+
+print(resultado)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
